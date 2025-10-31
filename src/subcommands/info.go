f62cb3c1980b22843795338752b2f1609df355d6
DoubleDotStudios <doubledotgames@gmail.com>
2025-10-31 21:47:19 +1300

    finish `info` subcommand
    Details:
    - Uses the `SearchPkg` function to find packages
    - Checks if more than one package by the same name exist across repos,
    if they do then prompt the user to select the one they wish to see
    
    Potential Improvements:
    - Gather packages with identical name, description and version together

--- src/subcommands/info.go
+++ src/subcommands/info.go
@@ -1,7 +1,10 @@
 package subcommands
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/Jguer/go-alpm/v2"
@@ -24,38 +27,67 @@ func PkgInfo(alpmHandle *alpm.Handle, args *InfoCmd) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	pkgList := []core.Package{}
 	pkgInfo := ""
 	selectedPkg := 0
-
 	for _, pkg := range pkgs {
 		if strings.EqualFold(pkg.Name, args.Name) {
 			pkgList = append(pkgList, pkg)
 		}
 	}
 
+	if len(pkgList) <= 0 {
+		nameStyle := lipgloss.NewStyle().Foreground(lipgloss.Blue)
+		return fmt.Sprintf("No packages named %s found", nameStyle.Render(args.Name)), nil
+	}
+
 	repoNameStyle := lipgloss.NewStyle().Foreground(lipgloss.Blue)
+	descStyle := lipgloss.NewStyle().
+		PaddingLeft(1)
 	borderStyle := lipgloss.NewStyle().
 		BorderStyle(lipgloss.RoundedBorder()).
-		Width(70).
 		PaddingLeft(1).
 		PaddingRight(1)
 
 	if len(pkgList) > 1 {
-		// TODO: Add user choice on which to view
-	} else if len(pkgList) <= 0 {
-		return fmt.Sprintf("No packages named %s found", repoNameStyle.Render(args.Name)), nil
+		nameStyle := lipgloss.NewStyle().Foreground(lipgloss.Blue)
+		pkgListString := fmt.Sprintf("There is more than one provider for %s:\n", nameStyle.Render(args.Name))
+		for i, pkg := range pkgList {
+			colorisedRepo := core.ColoriseByRepo(pkg.Repo, core.GetRepoColors())
+			pkgListString += fmt.Sprintf("%d) %s", i+1, colorisedRepo)
+			if i != len(pkgList)-1 {
+				pkgListString += "  |  "
+			}
+		}
+
+		pkgListString += lipgloss.
+			NewStyle().
+			Foreground(lipgloss.Blue).
+			Render("\n==> ")
+
+		fmt.Print(pkgListString)
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input := scanner.Text()
+		if input == "" {
+			input = "1"
+		}
+		selection, err := strconv.Atoi(input)
+		if err != nil || selection < 1 || selection > len(pkgList) {
+			return "", fmt.Errorf("invalid selection")
+		}
+		selectedPkg = selection - 1
 	}
 
 	pkg := pkgList[selectedPkg]
-	// repoColor := core.GetStyleForRepo(pkg.Repo, core.GetRepoColors())
-	// borderStyle = borderStyle.BorderForeground(repoColor)
-	// repoNameStyle := lipgloss.NewStyle().Foreground(repoColor)
-	borderStyle = borderStyle.BorderForeground(lipgloss.Blue)
-	pkgRepoName := repoNameStyle.Render(fmt.Sprintf("%s/%s - %s", pkg.Repo, pkg.Name, pkg.Version))
 
-	pkgInfo = borderStyle.Render(fmt.Sprintf("%s\n %s", pkgRepoName, pkg.Description))
+	pkgRepoPrefix := pkg.Repo
 
+	pkgDesc := descStyle.Render(pkg.Description)
+	pkgRepoName := repoNameStyle.Render(fmt.Sprintf("%s/%s - %s", pkgRepoPrefix, pkg.Name, pkg.Version))
+	borderStyle = borderStyle.
+		BorderForeground(lipgloss.Blue).
+		Width(len(pkgDesc) + 1)
+	pkgInfo = borderStyle.Render(fmt.Sprintf("%s\n%s", pkgRepoName, pkgDesc))
 	return pkgInfo, nil
 }
